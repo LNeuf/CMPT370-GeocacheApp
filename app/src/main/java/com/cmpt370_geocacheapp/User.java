91f3f895530541bfeb73140ff1ff27a1bf81f59e
Ashton Fritz (acf879) <acf879@mail.usask.ca>
2022-11-04 20:49:37 -0600

    Upload User class for geocache

---
+++ app/src/main/java/com/cmpt370_geocacheapp/User.java
@@ -0,0 +1,33 @@
+public class User {
+    private int userID;
+    private String password;
+    private String username;
+
+    public User(String username, String password, int userID) {
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+    }
+
+    public void changePassword(String currentPassword, String newPassword) {
+        if (currentPassword.equals(this.password)) {
+            this.password = newPassword;
+        }
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public int getUserID() {
+        return this.userID;
+    }
+
+    public static void main(String[] args) {
+        //TODO: testing for user
+    }
+}
