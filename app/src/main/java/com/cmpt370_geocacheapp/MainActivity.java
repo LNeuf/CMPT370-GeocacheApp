204aeaa782c6e50ea49b30cbb711c265dd8c71bf
Jesse Paterson <jesse.paterson@usask.ca>
2022-11-05 03:48:22 -0600

    Refactored and Cleaned up MainActivity.java to be more readable. Organized Current Location code to be more understandable

--- app/src/main/java/com/cmpt370_geocacheapp/MainActivity.java
+++ app/src/main/java/com/cmpt370_geocacheapp/MainActivity.java
@@ -4,20 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.Toast;
-
 import com.cmpt370_geocacheapp.databinding.ActivityHomeBinding;
-
-
 import android.Manifest.permission;
 import android.annotation.SuppressLint;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
@@ -28,20 +23,15 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
-
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 
-public class MainActivity extends AppCompatActivity implements IModelListener, ModelListener, OnMyLocationButtonClickListener,
-        OnMyLocationClickListener,
-        OnMapReadyCallback,
-        ActivityCompat.OnRequestPermissionsResultCallback{
+public class MainActivity extends AppCompatActivity implements IModelListener, ModelListener, OnMyLocationButtonClickListener, OnMyLocationClickListener, OnMapReadyCallback,ActivityCompat.OnRequestPermissionsResultCallback{
 
     ActivityHomeBinding binding;
     ListFragment listFragment;
@@ -49,20 +39,16 @@ public class MainActivity extends AppCompatActivity implements IModelListener, M
     ApplicationController controller;
     ApplicationModel model;
     InteractionModel iModel;
+    private GoogleMap gMap;
     /**
-     * Request code for location permission request.
-     *
-     * @see #onRequestPermissionsResult(int, String[], int[])
+     Request code for location permission request.
      */
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-
     /**
      * Flag indicating whether a requested permission has been denied after returning in {@link
      * #onRequestPermissionsResult(int, String[], int[])}.
      */
     private boolean permissionDenied = false;
-
-    private GoogleMap gMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,7 +64,6 @@ public class MainActivity extends AppCompatActivity implements IModelListener, M
         model = new ApplicationModel();
         iModel = new InteractionModel();
         controller = new ApplicationController();
-
         controller.setModel(model);
         controller.setInteractionModel(iModel);
 
@@ -89,15 +74,11 @@ public class MainActivity extends AppCompatActivity implements IModelListener, M
         // MVC linking
         listFragment.setModel(model);
         cacheCreateFragment.setModel(model);
-
         listFragment.setIModel(iModel);
-
-
         listFragment.setController(controller);
         cacheCreateFragment.setController(controller);
 
         model.addSubscriber(listFragment);
-
         iModel.addSubscriber(listFragment);
         iModel.addSubscriber(this);
 
@@ -111,7 +92,7 @@ public class MainActivity extends AppCompatActivity implements IModelListener, M
         fragmentTransaction.add(R.id.frame_layout, cacheCreateFragment);
         fragmentTransaction.commit();
 
-        // show map fragment by default on startup
+        // hide list and create fragments on startup - Google Map is always shown below
         hideFragment(listFragment);
         hideFragment(cacheCreateFragment);
 
@@ -140,8 +121,8 @@ public class MainActivity extends AppCompatActivity implements IModelListener, M
     public void onMapReady(@NonNull GoogleMap googleMap) {
         gMap = googleMap;
 
+        // Map setup with default settings
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
         gMap.getUiSettings().setZoomControlsEnabled(true);
         gMap.getUiSettings().setZoomGesturesEnabled(true);
         gMap.setInfoWindowAdapter(new CustomInfoWindow(this));
@@ -156,7 +137,7 @@ public class MainActivity extends AppCompatActivity implements IModelListener, M
         CameraPosition currentPos = CameraPosition.builder().target(usask).zoom(18).build();
         googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(currentPos));
 
-        // populate map with location markers
+        // populate map with test location markers. TODO: replace with nearby caches
         for (GeoCache cache : this.model.getFilteredCacheList())
         {
             gMap.addMarker(new MarkerOptions()
@@ -166,11 +147,155 @@ public class MainActivity extends AppCompatActivity implements IModelListener, M
 
         }
 
+        // Google map event handling & location enabling
         gMap.setOnMarkerClickListener(this::markerCLicked);
         gMap.setOnInfoWindowClickListener(this::infoWindowClicked);
         gMap.setOnMyLocationButtonClickListener(this);
         gMap.setOnMyLocationClickListener(this);
         enableMyLocation();
+    }
+
+    /**
+     * Shows a fragment
+     */
+    private void showFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.show(fragment);
+        fragmentTransaction.commit();
+    }
+
+    /**
+     * Hides a fragment
+     */
+    private void hideFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.hide(fragment);
+        fragmentTransaction.commit();
+    }
+
+    /**
+     * Handles the input checking for creating a new cache, makes controller create a cache if all inputs are good
+     * @param view
+     */
+    public void createCache(View view) {
+
+        EditText textInput = findViewById(R.id.cacheNameEditText);
+        String cacheName = textInput.getText().toString();
+        if (cacheName.equals("")) {
+            Toast.makeText(this, "Please enter a cache name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        textInput = findViewById(R.id.latitudeEditText);
+        float latitude;
+        try {
+            latitude = Float.parseFloat(textInput.getText().toString());
+        } catch (NumberFormatException e)
+        {
+            Toast.makeText(this, "Improper latitude entered", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (latitude < -90 || latitude > 90)
+            Toast.makeText(this, "Improper latitude entered", Toast.LENGTH_SHORT).show();
+
+        textInput = findViewById(R.id.longitudeEditText);
+        float longitude;
+        try {
+            longitude = Float.parseFloat(textInput.getText().toString());
+        } catch (NumberFormatException e)
+        {
+            Toast.makeText(this, "Improper longitude entered", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (longitude < -180 || longitude > 180)
+            Toast.makeText(this, "Improper longitude entered", Toast.LENGTH_SHORT).show();
+
+        // TODO fix cache size names
+        int cacheSize = -1;
+        RadioGroup group = findViewById(R.id.cacheSizeRadioGroup);
+
+        int selectedId = group.getCheckedRadioButtonId();
+        if (selectedId == R.id.cacheRadioSizeSmall)
+            cacheSize = 1;
+        else if (selectedId == R.id.cacheRadioSizeMed)
+            cacheSize = 2;
+        else if (selectedId == R.id.cacheRadioSizeLarge)
+            cacheSize = 3;
+
+        if (cacheSize == -1)
+        {
+            Toast.makeText(this, "Invalid cache size", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // TODO make sure difficulty is correct for real geocache object
+        SeekBar cacheDifficultySeekBar = findViewById(R.id.cacheDifficultySeekBar);
+        int cacheDifficulty = cacheDifficultySeekBar.getProgress();
+
+        // TODO: Assign creator name based on who is logged in
+        String cacheCreator = "Test Creator";
+
+        controller.createCache(cacheName, cacheSize, cacheDifficulty, 1, cacheCreator, latitude, longitude);
+    }
+
+    /**
+     * Event handling for when the custom Info Window is clicked
+     * @param marker - The marker clicked
+     */
+    private void infoWindowClicked(Marker marker) {
+        // TODO: Make a click on info window bring up cache detail fragment or window
+        Toast.makeText(this, "Show cache details for marker: " + marker.getSnippet(), Toast.LENGTH_SHORT).show();
+        GeoCache clickedCache = null;
+        for (GeoCache cache : model.getFilteredCacheList())
+        {
+            if (cache.getCacheID().equals(marker.getSnippet()))
+                clickedCache = cache;
+        }
+        if (clickedCache != null)
+            controller.setSelectedCache(clickedCache);
+    }
+
+    /**
+     * Event handling for when a marker is clicked
+     * @param marker - The marker
+     * @return - False to keep default behaviour
+     */
+    private boolean markerCLicked(Marker marker) {
+        Toast.makeText(this, "MarkerID: " + marker.getSnippet(), Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    @Override
+    public void iModelChanged() {
+        // selected new cache, move map to cache and zoom in on it
+        if (iModel.getCurrentlySelectedCache() != null)
+        {
+            gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(iModel.getCurrentlySelectedCache().getLatitude(),
+                            iModel.getCurrentlySelectedCache().getLongitude()), 17));
+
+        }
+        // hide other fragments
+        hideFragment(listFragment);
+        hideFragment(cacheCreateFragment);
+    }
+
+    @Override
+    public void modelChanged() {
+        // populate map with location markers
+        if (gMap != null) {
+            for (GeoCache cache : this.model.getFilteredCacheList()) {
+                gMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(cache.getLatitude(), cache.getLongitude()))
+                        .title(cache.getCacheName())
+                        .snippet(cache.getCacheID()));
+
+            }
+        }
     }
 
     /**
@@ -248,127 +373,4 @@ public class MainActivity extends AppCompatActivity implements IModelListener, M
                 .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
 
-    private void showFragment(Fragment fragment)
-    {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.show(fragment);
-        fragmentTransaction.commit();
-    }
-
-    private void hideFragment(Fragment fragment)
-    {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.hide(fragment);
-        fragmentTransaction.commit();
-    }
-
-    @Override
-    public void iModelChanged() {
-        // selected new cache, move map to cache and zoom in on it
-        if (iModel.getCurrentlySelectedCache() != null)
-        {
-            gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                    new LatLng(iModel.getCurrentlySelectedCache().getLatitude(),
-                            iModel.getCurrentlySelectedCache().getLongitude()), 17));
-
-        }
-        // hide other fragments
-        hideFragment(listFragment);
-        hideFragment(cacheCreateFragment);
-    }
-
-    public void createCache(View view) {
-
-        EditText textInput = findViewById(R.id.cacheNameEditText);
-        String cacheName = textInput.getText().toString();
-        if (cacheName.equals("")) {
-            Toast.makeText(this, "Please enter a cache name", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        textInput = findViewById(R.id.latitudeEditText);
-        float latitude;
-        try {
-            latitude = Float.parseFloat(textInput.getText().toString());
-        } catch (NumberFormatException e)
-        {
-            Toast.makeText(this, "Improper latitude entered", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if (latitude < -90 || latitude > 90)
-            Toast.makeText(this, "Improper latitude entered", Toast.LENGTH_SHORT).show();
-
-        textInput = findViewById(R.id.longitudeEditText);
-        float longitude;
-        try {
-            longitude = Float.parseFloat(textInput.getText().toString());
-        } catch (NumberFormatException e)
-        {
-            Toast.makeText(this, "Improper longitude entered", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if (longitude < -180 || longitude > 180)
-            Toast.makeText(this, "Improper longitude entered", Toast.LENGTH_SHORT).show();
-
-        // TODO fix cache size names
-        int cacheSize = -1;
-        RadioGroup group = findViewById(R.id.cacheSizeRadioGroup);
-
-        int selectedId = group.getCheckedRadioButtonId();
-        if (selectedId == R.id.cacheRadioSizeSmall)
-            cacheSize = 1;
-        else if (selectedId == R.id.cacheRadioSizeMed)
-            cacheSize = 2;
-        else if (selectedId == R.id.cacheRadioSizeLarge)
-            cacheSize = 3;
-
-        if (cacheSize == -1)
-        {
-            Toast.makeText(this, "Invalid cache size", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        // TODO make sure difficulty is correct for real geocache object
-        SeekBar cacheDifficultySeekBar = findViewById(R.id.cacheDifficultySeekBar);
-        int cacheDifficulty = cacheDifficultySeekBar.getProgress();
-
-        // TODO: Assign creator name based on who is logged in
-        String cacheCreator = "Test Creator";
-
-        controller.createCache(cacheName, cacheSize, cacheDifficulty, 1, cacheCreator, latitude, longitude);
-    }
-
-    private void infoWindowClicked(Marker marker) {
-        // TODO: Make a click on info window bring up cache detail fragment or window
-        Toast.makeText(this, "Show cache details for marker: " + marker.getSnippet(), Toast.LENGTH_SHORT).show();
-        GeoCache clickedCache = null;
-        for (GeoCache cache : model.getFilteredCacheList())
-        {
-            if (cache.getCacheID().equals(marker.getSnippet()))
-                clickedCache = cache;
-        }
-        if (clickedCache != null)
-            controller.setSelectedCache(clickedCache);
-    }
-
-    private boolean markerCLicked(Marker marker) {
-        Toast.makeText(this, "MarkerID: " + marker.getSnippet(), Toast.LENGTH_SHORT).show();
-        return false;
-    }
-
-    @Override
-    public void modelChanged() {
-        // populate map with location markers
-        if (gMap != null) {
-            for (GeoCache cache : this.model.getFilteredCacheList()) {
-                gMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(cache.getLatitude(), cache.getLongitude()))
-                        .title(cache.getCacheName())
-                        .snippet(cache.getCacheID()));
-
-            }
-        }
-    }
 }
