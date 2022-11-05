d963bd343d99e6197e8bd96df60e836936144256
Ashton Fritz (acf879) <acf879@mail.usask.ca>
2022-11-04 20:54:27 -0600

    Upload Comments for cache objects

---
+++ app/src/main/java/com/cmpt370_geocacheapp/CacheComment.java
@@ -0,0 +1,55 @@
+import java.util.Date;
+
+public class CacheComment {
+    private String commentBody;
+    final private User author;
+    final private long commentID;
+    final private String creationDate;
+    private String lastEditDate;
+    private boolean wasEdited;
+
+    public CacheComment(String commentBody, User author, long commentID) {
+        Date date = new Date();
+        this.commentBody = commentBody;
+        this.author = author;
+        this.commentID = commentID;
+        this.creationDate = date.toString();
+    }
+
+    public void replaceCommentBody(String commentBody) {
+        Date date = new Date();
+        this.commentBody = commentBody;
+        this.wasEdited = true;
+        this.lastEditDate = date.toString();
+    }
+
+    public String getCommentBody() {
+        return this.commentBody;
+    }
+
+    public long getCommentID() {
+        return this.commentID;
+    }
+
+    public User getAuthor() {
+        return this.author;
+    }
+
+    public String getCreationDate() {
+        return this.creationDate;
+    }
+
+    public String getLastEditDate() {
+        if (this.wasEdited) {
+            return this.lastEditDate;
+        }
+        else {
+            return this.creationDate;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        //TODO: Testing for CacheComment
+    }
+}
