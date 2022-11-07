37471582962c9e446b6bbc6374faa62d6bebde02
Ashton Fritz (acf879) <acf879@mail.usask.ca>
2022-11-06 22:40:27 -0600

    Replace PhysicalCacheBuilder.java

--- app/src/main/java/com/cmpt370_geocacheapp/PhysicalCacheBuilder.java
+++ app/src/main/java/com/cmpt370_geocacheapp/PhysicalCacheBuilder.java
@@ -9,14 +9,29 @@ public class PhysicalCacheBuilder implements CacheBuilder {
         this.database = databaseToLink;
     }
 
-    public void buildCache(String name, String author, long cacheID) {
+    public void buildCache(String name, User author, long cacheID) {
          this.cache = new CacheObject(name, author, cacheID);
     }
-    public void buildPhysicalCache(double latitude, double longitude, int cacheDif, int terrainDif, int cacheSize) {
-        this.database.addPhysicalCache(new PhysicalCacheObject(this.cache, latitude, longitude, cacheDif,terrainDif, cacheSize));
+    public void buildPhysicalCache(double latitude, double longitude, String difficulty, String terrain) {
+        this.database.addPhysicalCache(new PhysicalCacheObject(this.cache, latitude, longitude, difficulty,terrain));
     }
 
     public static void main(String[] args) {
-        //TODO: make tests for physicalCacheBuilder
+        CacheDatabase database = new CacheDatabase();
+        PhysicalCacheBuilder builder = new PhysicalCacheBuilder(database);
+        User user = new User("Jeff","chips800",300);
+        builder.buildCache("Cache1",user, 300);
+        builder.buildPhysicalCache(19.003,0.001,"Easy", "Mountainous");
+        int totalTests = 0;
+        int successfulTests = 0;
+
+        if (database.getPhysicalCache(300) == null) {
+            successfulTests--;
+            System.out.println("Did not build a physical cache.");
+        }
+        successfulTests++;
+        totalTests++;
+
+        System.out.println(successfulTests + " out of " + totalTests + " completed.");
     }
 }
